6ea795188ac30b762ed802a6c1e66bc4c8a99664
Srija <talamarlasrija@gmail.com>
2025-02-27 17:29:02 +0530

    feat/update and delete tasks

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,15 +1,69 @@
 import axios from "axios";
+import { useState } from "react";
 
 const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Task = ({ task, onTaskUpdated }) => {
 
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedTask, setEditedTask] = useState({
+        title: task.title,
+        description: task.description,
+    })
+
     const toggleTaskCompletion = async (id) => {
-        const response = await axios.patch(`${API_BASE_URL}/api/v1/tasks/${id}/toggle-completion`);
+        console.log("Updated Task Completion");
+        const res = await axios.patch(`${API_BASE_URL}/api/v1/tasks/${id}/toggle-completion`);
+        onTaskUpdated();
+    }
+
+    const deleteTask = async (id) => {
+        console.log("Delete task");
+        const res = await axios.delete(`${API_BASE_URL}/api/v1/tasks/${id}`)
+        onTaskUpdated();
+    }
+
+    const updateTask = async (e) => {
+        e.preventDefault();
+        console.log("Update Task");
+        const res = await axios.put(`${API_BASE_URL}/api/v1/tasks/${task.id}`, editedTask)
+        setIsEditing(false);
         onTaskUpdated();
     }
     return (
         <>
+            {isEditing && (
+                <div>
+                    <p>Update task</p>
+                    <form onSubmit={(e) => updateTask(e)}>
+                        <div>
+                            <label htmlFor="title">Title</label>
+                            <input
+                                id="title"
+                                type="text"
+                                name="title"
+                                value={editedTask.title}
+                                onChange={(e) => setEditedTask({ ...editedTask, title: e.target.value })}
+                                required
+                            />
+                        </div>
+
+                        <div>
+                            <label htmlFor="description">Description</label>
+                            <textarea
+                                id="description"
+                                name="description"
+                                rows="4"
+                                value={editedTask.description}
+                                onChange={(e) => setEditedTask({ ...editedTask, description: e.target.value })}
+                            />
+                        </div>
+                        <button type="submit">Save</button>
+                        <button onClick={() => setIsEditing(false)}>Cancel</button>
+                    </form>
+                </div >
+            )}
+
             <li key={task.id}>
                 <div className="toggle-task-completion">
                     <label htmlFor="toggle-task-completion"></label>
@@ -23,6 +77,10 @@ const Task = ({ task, onTaskUpdated }) => {
                 </div>
                 <strong>{task.title}</strong> - {task.description} -{" "}
                 {task.completed ? "✅ Completed" : "❌ Pending"}
+                <div>
+                    <button onClick={() => setIsEditing(true)}>Edit</button>
+                    <button onClick={() => deleteTask(task.id)}>Delete</button>
+                </div>
             </li>
         </>
     )

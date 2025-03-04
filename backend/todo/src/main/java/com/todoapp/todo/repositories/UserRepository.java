2457e8fd7106c9c11490e8870eb3520808b553fb
Srija <talamarlasrija@gmail.com>
2025-03-04 23:17:58 +0530

    feat/User register api

---
+++ backend/todo/src/main/java/com/todoapp/todo/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.todoapp.todo.repositories;
+
+import com.todoapp.todo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ backend/todo/src/main/java/com/todoapp/todo/services/UserService.java
@@ -0,0 +1,16 @@
+package com.todoapp.todo.services;
+
+import com.todoapp.todo.models.User;
+import com.todoapp.todo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+}
